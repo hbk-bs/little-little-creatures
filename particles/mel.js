16cc967e8027cf18bb6e7c4852e1c85aa21f0feb
mel <148953950+melmelllll@users.noreply.github.com>
2023-12-10 15:21:26 +0100

    Update mel.js

--- particles/mel.js
+++ particles/mel.js
@@ -10,16 +10,16 @@ class mel extends Particle {
     noStroke();
     c = color('hsba(160, 100%, 50%, 0.5)');
     fill(c);
-    // Draw a star shape relative to the current position (this.pos)
+    // Draw a star shape 
     beginShape();
-    vertex(this.pos.x, this.pos.y - 20);
-    vertex(this.pos.x + 5, this.pos.y - 5);
-    vertex(this.pos.x + 20, this.pos.y);
-    vertex(this.pos.x + 5, this.pos.y + 5);
-    vertex(this.pos.x, this.pos.y + 20);
-    vertex(this.pos.x - 5, this.pos.y + 5);
-    vertex(this.pos.x - 20, this.pos.y);
-    vertex(this.pos.x - 5, this.pos.y - 5);
+    vertex(this.pos.x, this.pos.y - 50);
+    vertex(this.pos.x + 10, this.pos.y - 10);
+    vertex(this.pos.x + 50, this.pos.y);
+    vertex(this.pos.x + 10, this.pos.y + 10);
+    vertex(this.pos.x, this.pos.y + 50);
+    vertex(this.pos.x - 10, this.pos.y + 10);
+    vertex(this.pos.x - 50, this.pos.y);
+    vertex(this.pos.x - 10, this.pos.y - 10);
     endShape(CLOSE);
     pop();
   }
