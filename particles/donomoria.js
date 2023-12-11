5c478456094bc98ac11ac1c3df04251892a24739
donomoria <m.kukharenka@hbk-bs.de>
2023-12-11 04:37:25 +0300

    succed

--- particles/donomoria.js
+++ particles/donomoria.js
@@ -2,4 +2,9 @@ class DonoParticle extends Particle{
     constructor(pos) {
         super(pos);
       }
+}  display() {
+    fill(this.color);
+    noStroke();
+    rect(this.x, this.y, 50, 50);
+  }
 }
