b8be6409b086dd0050175cf6136f04d52e9d820c
Philkng <p.koenig@hbk-bs.de>
2023-12-11 14:11:26 +0100

    Phils

---
+++ particles/phillys.js
@@ -0,0 +1,14 @@
+class PhillysParticle extends Particle {
+    constructor(pos) {
+      super(pos);
+    }
+    
+    display() {
+      push();
+      noStroke();
+      fill(0);
+      ellipse(this.pos.x, this.pos.y, 20, 20);
+      pop();
+    }
+  }
+  
