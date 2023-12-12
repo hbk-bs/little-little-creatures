de2232828eef415e8caf34042393d04597aaaa04
ff6347 <hash@ff6347.email>
2023-12-12 10:25:02 +0100

    fix(donomoria): Make it work

--- particles/donomoria.js
+++ particles/donomoria.js
@@ -1,10 +1,12 @@
-class DonoParticle extends Particle{
-    constructor(pos) {
-        super(pos);
-      }
-}  display() {
+class DonoParticle extends Particle {
+  color;
+  constructor(pos) {
+    super(pos);
+    this.color = random(255);
+  }
+  display() {
     fill(this.color);
     noStroke();
-    rect(this.x, this.y, 50, 50);
+    rect(this.pos.x, this.pos.y, 50, 50);
   }
 }
