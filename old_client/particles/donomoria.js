7f902b1c220eb70608702f34491f5569da3f932e
donomoria <m.kukharenka@hbk-bs.de>
2024-01-15 21:18:52 +0100

    klappppt es?

--- old_client/particles/donomoria.js
+++ old_client/particles/donomoria.js
@@ -1,12 +1,27 @@
 class DonoParticle extends Particle {
-	color;
+
 	constructor(pos) {
-		super(pos);
-		this.color = random(255);
+	  super(pos);
+	  this.color = color(random(255), random(255), random(255));
 	}
+  
+	setColor() {
+	  this.color = color(random(255), random(255), random(255));
+	}
+  
 	display() {
-		fill(this.color);
-		noStroke();
-		rect(this.pos.x, this.pos.y, 50, 50);
+	  fill(this.color);
+	  noStroke();
+	  rect(this.pos.x, this.pos.y, 50, 50);
+	  const distance = dist(mouseX, mouseY, this.pos.x, this.pos.y);
+	
+	  if (distance < 200) {
+		const angle = atan2(mouseY - this.pos.y, mouseX - this.pos.x);
+		const newX = this.pos.x + cos(angle) * 10;
+		const newY = this.pos.y + sin(angle) * 10;
+		this.pos.x = newX;
+		this.pos.y = newY;
+		this.setColor();
+	  }
 	}
-}
+  }
