837e9c9cad031f1e42f37656f72f4438d3a7d17e
ff6347 <hash@ff6347.email>
2024-01-17 22:13:41 +0100

    fix(server): Handle dns lookup error

--- server/lib/util.ts
+++ server/lib/util.ts
@@ -1,9 +1,15 @@
 import os from "node:os";
 import dns from "node:dns/promises";
+
 export async function getHostnameAndIP() {
-	// Get hostname
-	const hostname = os.hostname();
-	// Get IP
-	const { address } = await dns.lookup(hostname);
-	return { hostname, address };
+	try {
+		// Get hostname
+		const hostname = os.hostname();
+		// Get IP
+		const { address } = await dns.lookup(hostname);
+
+		return { hostname, address, error: null };
+	} catch (error) {
+		return { error };
+	}
 }
